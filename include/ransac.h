0e504919163a75ef312a096ce5dcabd1f8e4e3fd
Jamil Drareni <urbeller@macbookpro>
2019-12-02 14:14:35 -0500

    expand tabs.

--- include/ransac.h
+++ include/ransac.h
@@ -10,165 +10,165 @@ using namespace std;
 
 namespace AnySac
 {
-	// A driver must define the following methods:
-	//
-	// int model_size() const;
-	// int data_size() const;
-	// bool fit_model(const vector<int> &indices, vector<ModelType> &models);
-	// int count_inliers(const ModelType &model);
-	// bool check_subset(const vector<int> &indices);
-	// bool has_generator();
-	// bool get_subset(vector<int> &indices);
+  // A driver must define the following methods:
+  //
+  // int model_size() const;
+  // int data_size() const;
+  // bool fit_model(const vector<int> &indices, vector<ModelType> &models);
+  // int count_inliers(const ModelType &model);
+  // bool check_subset(const vector<int> &indices);
+  // bool has_generator();
+  // bool get_subset(vector<int> &indices);
 
-	template< typename DriverType, typename ModelType>
-		class Ransac
-		{
-			public:
+  template< typename DriverType, typename ModelType>
+    class Ransac
+    {
+      public:
 
-				Ransac( DriverType &k , double conf_=0.99) : 
-					driver(k), 
-					maxAttempts(300), 
-					maxIters(300), 
-					conf(conf_),
-					rnd_gen((unsigned int)time(0)),
-					rnd_dist(0, k.data_size() - 1)
+        Ransac( DriverType &k , double conf_=0.99) : 
+          driver(k), 
+          maxAttempts(300), 
+          maxIters(300), 
+          conf(conf_),
+          rnd_gen((unsigned int)time(0)),
+          rnd_dist(0, k.data_size() - 1)
 
-			{
-			}
+      {
+      }
 
-				// Solve the problem.
-				// Input: a model object (driver).
-				// Returns: number of inliers.
-				int solve(ModelType &model)
-				{
+        // Solve the problem.
+        // Input: a model object (driver).
+        // Returns: number of inliers.
+        int solve(ModelType &model)
+        {
 
-					int         modelSize = driver.model_size();
-					int         dataSize = driver.data_size();
-					int         niter    = maxIters;
-					int         maxCount = 0;
-					vector<int> ndices(modelSize);
-					vector<ModelType> models;
-					bool has_generator = driver.has_generator();
+          int         modelSize = driver.model_size();
+          int         dataSize = driver.data_size();
+          int         niter    = maxIters;
+          int         maxCount = 0;
+          vector<int> ndices(modelSize);
+          vector<ModelType> models;
+          bool has_generator = driver.has_generator();
 
-					for (int iter=0; iter<niter; iter++)
-					{
-						if ( dataSize >= modelSize )
-						{
-
-
-							bool found = (has_generator) ? driver.get_subset(ndices) : get_subset(ndices);
-
-							if( !found )
-							{
-								if( iter == 0 )
-									return 0;
-
-								break;
-							}
-
-							// We got a valid subset. Estimate a model.
-							if( !driver.fit_model ( ndices, models ) )
-							{
-								//return 0;
-							}
-
-							// Find the best model.
-							for(unsigned int m = 0, len = models.size(); m<len; m++)
-							{
-								int count = driver.count_inliers( models[m] );
-
-								if( count > std::max(maxCount, modelSize-1) )
-								{
-									maxCount = count;
-
-									model = models[m];
-									niter = update_niters(conf, double(dataSize-count)/dataSize, modelSize, niter);
-								}
-							}
-
-						} 
-						else 
-						{
-							cerr<<__FUNCTION__<<": dataSize is smaller than model size.\n";
-							break;
-						}
-
-					}
-
-					// Update the inliers count and the inliers mask.
-					maxCount = driver.count_inliers( model );
-					return maxCount;
-				}
-
-			private:
-
-				// Pick a random combination of indices.
-				bool get_subset( vector<int> &pickedIndex)
-				{
-
-					int j, i=0, iter=0;
-					int dataSize  = driver.data_size();
-					int modelSize = driver.model_size();
-
-					for (; iter< maxAttempts; iter++)
-					{
-
-						for (i=0; i<modelSize && iter<maxAttempts; )
-						{
-							pickedIndex[i] = rnd_dist(rnd_gen);
-
-							for (j = 0; j < i; j++ )
-								if (pickedIndex[j] == pickedIndex[i])
-									break;
-
-							if (j < i)
-								continue;
-
-							i++;
-						}
-
-						if (i==modelSize && !driver.check_subset(pickedIndex))
-							continue;
-
-						break;
-					}
-
-					return i == modelSize && iter < maxAttempts;
-				}
+          for (int iter=0; iter<niter; iter++)
+          {
+            if ( dataSize >= modelSize )
+            {
 
 
-				// Update the minimal number of ransac iterations.
-				int update_niters( double p, double ep, int modelPoints, int maxIter) const
-				{
+              bool found = (has_generator) ? driver.get_subset(ndices) : get_subset(ndices);
 
-					p   = std::max(p, 0.0);
-					p   = std::min(p, 1.0);
-					ep  = std::max(ep, 0.0);
-					ep  = std::min(ep, 1.0);
+              if( !found )
+              {
+                if( iter == 0 )
+                  return 0;
 
-					// avoid inf's & nan's
-					double num = std::max(1.0 - p, std::numeric_limits<double>::min() );
-					double denom = 1. - pow(1.0 - ep,modelPoints);
-					if( denom < std::numeric_limits<double>::min() )
-						return 0;
+                break;
+              }
 
-					num = log(num);
-					denom = log(denom);
+              // We got a valid subset. Estimate a model.
+              if( !driver.fit_model ( ndices, models ) )
+              {
+                //return 0;
+              }
 
-					return denom >= 0 || -num >= maxIter*(-denom) ?
-						maxIter : round(num/denom);
-				}
+              // Find the best model.
+              for(unsigned int m = 0, len = models.size(); m<len; m++)
+              {
+                int count = driver.count_inliers( models[m] );
+
+                if( count > std::max(maxCount, modelSize-1) )
+                {
+                  maxCount = count;
+
+                  model = models[m];
+                  niter = update_niters(conf, double(dataSize-count)/dataSize, modelSize, niter);
+                }
+              }
+
+            } 
+            else 
+            {
+              cerr<<__FUNCTION__<<": dataSize is smaller than model size.\n";
+              break;
+            }
+
+          }
+
+          // Update the inliers count and the inliers mask.
+          maxCount = driver.count_inliers( model );
+          return maxCount;
+        }
+
+      private:
+
+        // Pick a random combination of indices.
+        bool get_subset( vector<int> &pickedIndex)
+        {
+
+          int j, i=0, iter=0;
+          int dataSize  = driver.data_size();
+          int modelSize = driver.model_size();
+
+          for (; iter< maxAttempts; iter++)
+          {
+
+            for (i=0; i<modelSize && iter<maxAttempts; )
+            {
+              pickedIndex[i] = rnd_dist(rnd_gen);
+
+              for (j = 0; j < i; j++ )
+                if (pickedIndex[j] == pickedIndex[i])
+                  break;
+
+              if (j < i)
+                continue;
+
+              i++;
+            }
+
+            if (i==modelSize && !driver.check_subset(pickedIndex))
+              continue;
+
+            break;
+          }
+
+          return i == modelSize && iter < maxAttempts;
+        }
 
 
-				DriverType &driver;
-				int         maxAttempts;
-				int         maxIters;
-				double      conf;
+        // Update the minimal number of ransac iterations.
+        int update_niters( double p, double ep, int modelPoints, int maxIter) const
+        {
 
-				// For combinations generation.
-				std::mt19937 rnd_gen;
-				std::uniform_int_distribution<> rnd_dist;
-		};
+          p   = std::max(p, 0.0);
+          p   = std::min(p, 1.0);
+          ep  = std::max(ep, 0.0);
+          ep  = std::min(ep, 1.0);
+
+          // avoid inf's & nan's
+          double num = std::max(1.0 - p, std::numeric_limits<double>::min() );
+          double denom = 1. - pow(1.0 - ep,modelPoints);
+          if( denom < std::numeric_limits<double>::min() )
+            return 0;
+
+          num = log(num);
+          denom = log(denom);
+
+          return denom >= 0 || -num >= maxIter*(-denom) ?
+            maxIter : round(num/denom);
+        }
+
+
+        DriverType &driver;
+        int         maxAttempts;
+        int         maxIters;
+        double      conf;
+
+        // For combinations generation.
+        std::mt19937 rnd_gen;
+        std::uniform_int_distribution<> rnd_dist;
+    };
 }
 
 #endif
--- src/line_driver.cxx
+++ src/line_driver.cxx
@@ -10,150 +10,150 @@ using namespace std;
 // Struct for 2d points.
 struct Pt2d
 {
-	Pt2d(double x_, double y_) : x(x_), y(y_){}
-	Pt2d() :x(0.), y(0.){}
+  Pt2d(double x_, double y_) : x(x_), y(y_){}
+  Pt2d() :x(0.), y(0.){}
 
-	double x, y;
+  double x, y;
 
-	double dist(const Pt2d &other) const { return sqrt( pow(x - other.x,2) + pow(y - other.y,2));}
+  double dist(const Pt2d &other) const { return sqrt( pow(x - other.x,2) + pow(y - other.y,2));}
 };
 
 class LineDriver
 {
 
 public:
-	LineDriver(const vector<Pt2d> &pts_, double th_) : pts(pts_), threshold(th_),
-			rnd_gen((unsigned int)time(0)), rnd_dist(0, pts.size() - 1)
-	{}
+  LineDriver(const vector<Pt2d> &pts_, double th_) : pts(pts_), threshold(th_),
+      rnd_gen((unsigned int)time(0)), rnd_dist(0, pts.size() - 1)
+  {}
 
-	// Required methods.
-	int model_size() const;
-	int data_size() const;
-	bool fit_model(const vector<int> &indices, vector<Pt2d> &models);
-	int count_inliers(const Pt2d &model);
-	bool check_subset(const vector<int> &indices);
-	bool get_subset( vector<int> &pickedIndex);
-	bool has_generator() const
-	{
-		return true; // set this to false to use Ransac<> generator.
-	}
+  // Required methods.
+  int model_size() const;
+  int data_size() const;
+  bool fit_model(const vector<int> &indices, vector<Pt2d> &models);
+  int count_inliers(const Pt2d &model);
+  bool check_subset(const vector<int> &indices);
+  bool get_subset( vector<int> &pickedIndex);
+  bool has_generator() const
+  {
+    return true; // set this to false to use Ransac<> generator.
+  }
 
 private:
-	const vector<Pt2d> &pts;
-	double threshold;
-	const int modelSize = 2;
+  const vector<Pt2d> &pts;
+  double threshold;
+  const int modelSize = 2;
 
-	// To show an example of a user-defined indices-generator.
-	std::mt19937 rnd_gen;
-	std::uniform_int_distribution<> rnd_dist;
+  // To show an example of a user-defined indices-generator.
+  std::mt19937 rnd_gen;
+  std::uniform_int_distribution<> rnd_dist;
 };
 
 int main(int argc, char **argv)
 {
 
-	std::random_device rd;  
-	std::mt19937 gen(rd()); 
-	std::uniform_real_distribution<> dis(-100.0, 100.0);
+  std::random_device rd;  
+  std::mt19937 gen(rd()); 
+  std::uniform_real_distribution<> dis(-100.0, 100.0);
 
-	// Generate noisy points on a line.
-	vector<Pt2d> pts(100);
-	double slope = 10, b = 5;
+  // Generate noisy points on a line.
+  vector<Pt2d> pts(100);
+  double slope = 10, b = 5;
 
-	for (int i = 0; i < pts.size(); ++i)
-	{
-		pts[i].x = dis(gen);
+  for (int i = 0; i < pts.size(); ++i)
+  {
+    pts[i].x = dis(gen);
 
-		double noise = (dis(gen) / 200.0) ;
-		pts[i].y = pts[i].x * slope + b +  noise;
-	}
+    double noise = (dis(gen) / 200.0) ;
+    pts[i].y = pts[i].x * slope + b +  noise;
+  }
 
 
-	// Init the driver.
-	LineDriver ld(pts, 0.01);
-	
-	// Solve the problem.
-	Pt2d sol;
-	AnySac::Ransac<LineDriver, Pt2d> sac(ld);
-	sac.solve(sol);
+  // Init the driver.
+  LineDriver ld(pts, 0.01);
+  
+  // Solve the problem.
+  Pt2d sol;
+  AnySac::Ransac<LineDriver, Pt2d> sac(ld);
+  sac.solve(sol);
 
-	cout << sol.x << " " << sol.y << endl;
+  cout << sol.x << " " << sol.y << endl;
 }
 
 
 
-	int LineDriver::model_size() const 
-	{ 
-		return modelSize;
-	}
+  int LineDriver::model_size() const 
+  { 
+    return modelSize;
+  }
 
-	int LineDriver::data_size() const 
-	{ 
-		return pts.size();
-	}
-	
-	bool LineDriver::fit_model(const vector<int> &indices, vector<Pt2d> &models)
-	{
-		const Pt2d &p0 = pts[indices[0]];
-		const Pt2d &p1 = pts[indices[1]];
+  int LineDriver::data_size() const 
+  { 
+    return pts.size();
+  }
+  
+  bool LineDriver::fit_model(const vector<int> &indices, vector<Pt2d> &models)
+  {
+    const Pt2d &p0 = pts[indices[0]];
+    const Pt2d &p1 = pts[indices[1]];
 
-		if (p0.dist(p1) < threshold)
-			return false;
+    if (p0.dist(p1) < threshold)
+      return false;
 
-		models.resize(1);
+    models.resize(1);
 
-		double slope = (p1.y - p0.y) / (p1.x - p0.x);
-		double b = p0.y - slope * p0.x;
+    double slope = (p1.y - p0.y) / (p1.x - p0.x);
+    double b = p0.y - slope * p0.x;
 
-		models[0] = Pt2d(slope, b);
+    models[0] = Pt2d(slope, b);
 
-		return true;
-	}
+    return true;
+  }
 
 
-	int LineDriver::count_inliers(const Pt2d &model)
-	{
-		int inliers = 0;
+  int LineDriver::count_inliers(const Pt2d &model)
+  {
+    int inliers = 0;
 
-		for (int i = 0; i < pts.size(); ++i)
-		{
-			const Pt2d &p = pts[i];
+    for (int i = 0; i < pts.size(); ++i)
+    {
+      const Pt2d &p = pts[i];
 
-			double dist = fabs(p.x * model.x + model.y - p.y);
-			inliers += (dist < threshold);
-		}
+      double dist = fabs(p.x * model.x + model.y - p.y);
+      inliers += (dist < threshold);
+    }
 
-		return inliers;
-	}
+    return inliers;
+  }
 
-	bool LineDriver::check_subset(const vector<int> &indices)
-	{
-		// Consider only first-n indices.
-		if(indices.size() < modelSize || indices[0] == indices[1])
-			return false;
+  bool LineDriver::check_subset(const vector<int> &indices)
+  {
+    // Consider only first-n indices.
+    if(indices.size() < modelSize || indices[0] == indices[1])
+      return false;
 
-		const Pt2d &p0 = pts[indices[0]];
-		const Pt2d &p1 = pts[indices[1]];
+    const Pt2d &p0 = pts[indices[0]];
+    const Pt2d &p1 = pts[indices[1]];
 
-		if (p0.dist(p1) < threshold)
-			return false;
-		
-		return true;
-	}
+    if (p0.dist(p1) < threshold)
+      return false;
+    
+    return true;
+  }
 
 
 
 bool LineDriver::get_subset( vector<int> &indices)
 {
-	indices.resize(2, 0);
-	int max_iters = 10;
+  indices.resize(2, 0);
+  int max_iters = 10;
 
-	int niter = 0;
-	while (indices[0] == indices[1] && niter < max_iters)
-	{
-		indices[0] = rnd_dist(rnd_gen);
-		indices[1] = rnd_dist(rnd_gen);
-		++niter;
-	}
+  int niter = 0;
+  while (indices[0] == indices[1] && niter < max_iters)
+  {
+    indices[0] = rnd_dist(rnd_gen);
+    indices[1] = rnd_dist(rnd_gen);
+    ++niter;
+  }
 
-	return niter < max_iters;
+  return niter < max_iters;
 }
